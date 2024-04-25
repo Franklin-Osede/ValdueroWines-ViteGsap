2ff075af6723827922d5799824882949abadd1c6
Franklin <franklin.op@hotmail.com>
2024-04-25 15:04:46 +0200

    setting tailwing and css

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        red: "#b11226", // wine red color
+        cream: {
+          DEFAULT: "#f5f5dc", // cream color
+          100: "#fafaf0",
+          200: "#f0ead6",
+          300: "#e5dbcd",
+        },
+        gold: "#ffd700", // gold color
+      },
+    },
   },
   plugins: [],
 }
